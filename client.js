49da43fe244e5eedf5c0be1623903b19b8e08a3a
dtsaknakis <dtsaknakis.web@gmail.com>
2022-10-24 05:52:15 +0300

    Added parcel, need to add notes on using the library

---
+++ client.js
@@ -0,0 +1,6 @@
+// Import library function as ES6 module.
+import { to_units } from "@dtsaknakis/bonta-tools";
+
+const dysport_units = to_units("dysport"); // should be 500
+console.log(`Dysport has ${dysport_units} Units per vial`);
+
---
+++ server.js
@@ -0,0 +1,6 @@
+// Import library function via CommonJS
+// Run via Node (node server.js)
+const { to_units } = require("@dtsaknakis/bonta-tools");
+
+console.log(`Bocouture has ${to_units("Bocouture")} Units per vial.`);
+
